f913cef9e5043705ab6c7361bf8ae7aa17e75ea6
Kunal Pandey <pandeykunal2097@gmail.com>
2022-04-30 15:56:31 +0530

    created contact table and some major changes

--- app/controllers/EmailDeploymentController.js
+++ app/controllers/EmailDeploymentController.js
@@ -1,8 +1,9 @@
 const db = require("../models");
-const Tutorial = db.tutorials;
+//taking model
+const EmailDeployment = db.EmailDeployment;
 const Op = db.Sequelize.Op;
 
-export default {
+module.exports = {
     getDeployemntById: async (req, res) => { },
     createDeployment: async (req, res) => { },
     DeleteDeployemntById: async (req, res) => { },
